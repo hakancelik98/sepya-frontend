b57e2d160bbea0a3ae470cea7e960f77bb588b09
Hakan Çelik <74689538+hakancelik98@users.noreply.github.com>
2026-06-05 02:16:29 +0300

    kampanya ürün panel v1.02

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -176,6 +176,7 @@ export default function ProductList({ filteredProducts, handleEdit, handleDelete
                             );
                         })
                     ) : (
+
                         <tr>
                             <td colSpan={13} className="px-4 py-8 text-center text-slate-400">
                                 <span className="text-[12px] font-bold">Ürün bulunamadı</span>
